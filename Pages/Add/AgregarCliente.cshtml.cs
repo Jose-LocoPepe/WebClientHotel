c5a4e618e37df5fa15944001d14e4ec3653e73d3
Ertinho <105566974+Ertinho@users.noreply.github.com>
2024-12-18 22:24:58 -0300

    feat(AgregarCliente): add env variable

--- Pages/Add/AgregarCliente.cshtml.cs
+++ Pages/Add/AgregarCliente.cshtml.cs
@@ -3,24 +3,37 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
 using WebClientHotel;
+using DotNetEnv;
 
 namespace WebClientHotel.Pages.Add
 {
     public class AgregarClienteModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-
         public async Task OnPostAsync(string rut, string nombre, string apellido)
         {
-            // TODO: add the user to the database
+            try
+            {
+                DotNetEnv.Env.Load();
+                var grpcServerUrl = Environment.GetEnvironmentVariable("GRPC_SERVER_URL");
+                if (string.IsNullOrEmpty(grpcServerUrl))
+                {
+                    throw new InvalidOperationException("GRPC_SERVER_URL is not set in .env file");
+                }
 
-            // TODO: The port number must match the port of the ServidorgRPC.
-            using var channel = GrpcChannel.ForAddress("http://localhost:5165");
-            var client = new Greeter.GreeterClient(channel);
-            var reply = await client.AgregarClienteAsync(
-                              new AgregarRequest { Rut = rut, Nombre = nombre, Apellido = apellido });
+                // TODO: add the user to the database
+
+
+                // TODO: The port number must match the port of the ServidorgRPC.
+                using var channel = GrpcChannel.ForAddress(grpcServerUrl);
+                var client = new Greeter.GreeterClient(channel);
+                var reply = await client.AgregarClienteAsync(
+                                new AgregarRequest { Rut = rut, Nombre = nombre, Apellido = apellido });
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error al agregar cliente");
+                throw;
+            }
         }
     }
 }
